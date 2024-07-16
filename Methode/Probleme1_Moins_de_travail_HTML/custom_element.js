2bdbee033c7b3aaf21c434302dbe9e1be795f5db
Francois-xavier Coindreau <fcoindre@student.42.fr>
2024-07-16 23:02:26 +0200

    custom element improved

--- Methode/Probleme1_Moins_de_travail_HTML/custom_element.js
+++ Methode/Probleme1_Moins_de_travail_HTML/custom_element.js
@@ -5,7 +5,14 @@ class MyOwnP extends HTMLParagraphElement
 	connectedCallback()
 	{
 		console.log("Custom p added to page.");
+		this.addEventListener('click', this.handleClick);
 	}
+
+	handleClick()
+	{
+		alert("Vous avez cliqué sur un p custom !");
+	}
+
 }
 
 customElements.define("my-own-p", MyOwnP, { extends: "p" });
